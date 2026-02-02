f701b721b3cb52b33c76c93fbaa772fea0bfff6e
clementmtai-png <clementmtai@gmail.com>
2026-02-02 03:00:05 +0300

    Add files via upload

---
+++ crash_game_platform_stk/js/config.js
@@ -0,0 +1,24 @@
+const CONFIG = {
+    ADMIN_COMMAND: 'admin.pro.aviator', // Hidden trigger
+    DEFAULT_BALANCE: 0, // Balance stays 0 until a payment is made
+    // Deposit button: LEAVE EMPTY to use the internal STK Push modal
+    DEPOSIT_LINK: '',
+    // Pesapal link: LEAVE EMPTY to use the STK Push API flow
+    PESAPAL_PAYMENT_LINK: '',
+    MIN_DEPOSIT: 500,
+    CHAT_MESSAGES: [
+        "Big win incoming!",
+        "Let's goooo 🚀",
+        "Wait for 2x guys",
+        "Just cashed out @ 1.8x, lucky me",
+        "Flew away too fast :(",
+        "Anyone betting on this round?",
+        "This is crazy!",
+        "Predictor v14 is working well",
+        "Just deposited, feeling lucky",
+        "Don't get greedy friends"
+    ],
+    USERNAMES: ["RocketMan", "SkyKing", "AviatorQueen", "MoonShot", "BigBettor", "CasualGamer", "LuckyStike", "ProfitHunter", "KenyaEagle", "ZimFalcon"],
+    MIN_BET: 1.00,
+    GAME_COOLDOWN: 5000, // 5 seconds between rounds
+};
